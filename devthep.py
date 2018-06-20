c8e00f0dbb34d3ef972c19afea5b3f39aaa6a339
Thepnathi Stephenson <Thepnathi@hotmail.com>
2018-06-20 22:03:37 +0100

    add blog category and more routes

--- devthep.py
+++ devthep.py
@@ -5,7 +5,11 @@ import datetime
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# get current time in dd/mm/yyyy
 date = datetime.datetime.today().strftime('%d-%m-%Y')
+
+blogCategory = ['Programming', 'Web Development', 'Computer Science', 'Everyday Life', 'Travelling', 'Careers',
+'Software Engineer', 'Mathematics']
 
 exampleBlog = [
     {
@@ -22,13 +26,25 @@ exampleBlog = [
 
 # Function below will render/load the html template
 @app.route("/")
-@app.route("/blog")
-def blog():
+@app.route("/home")
+def home():
     return render_template('index.html', name="Thepnathi")
 
 @app.route("/about")
 def about():
     return render_template('about-me.html')
+
+@app.route("/blog")
+def blog():
+    return "Blog page"
+
+@app.route("/category")
+def category():
+    return "Catergories for different blog"
+
+@app.route("/contact")
+def conbtact():
+    return "How to contact me"
 
 # Python3 devthep.py 
 # to run on debug mode
