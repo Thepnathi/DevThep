2568c0aa751812d32353a921a4956aa85cff38e5
Thepnathi Stephenson <Thepnathi@hotmail.com>
2018-08-01 14:11:21 +0100

    Add Bulma grid system to home page

--- devthep/views.py
+++ devthep/views.py
@@ -16,11 +16,19 @@ examplePost = [
         'Image': 'https://images.unsplash.com/photo-1530728327726-b504480e42ec?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2e330bf90dfed0f3051eda7c8aa04784&auto=format&fit=crop&w=2089&q=80',
         'Author': 'Thepnathi',
         'Date': "13/08/2017",
+        'Summary': "The biggest whale in the world found in Africa!",
         'Content': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
     },
     {
         'Title': 'Big Fat Burger!',
-        'Image': 'https://images.unsplash.com/photo-1530728327726-b504480e42ec?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2e330bf90dfed0f3051eda7c8aa04784&auto=format&fit=crop&w=2089&q=80',
+        'Image': 'https://images.unsplash.com/photo-1533057991625-38cfd50afcb1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e3b123c7e617989df4fcdf2b68aa077e&auto=format&fit=crop&w=3149&q=80',
+        'Author': 'Thepnathi',
+        'Date': "13/08/2017",
+        'Content': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },
+    {
+        'Title': 'Big Fat Burger!',
+        'Image': 'https://images.unsplash.com/photo-1466978913421-dad2ebd01d17?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=719292378935d0f9168f11f9ad00558d&auto=format&fit=crop&w=1567&q=80',
         'Author': 'Thepnathi',
         'Date': "13/08/2017",
         'Content': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
@@ -32,9 +40,11 @@ examplePost = [
 @app.route("/home")
 def home():
     random_quote = get_random_quote()
+    print ("SERVER SUCC")
+    print (examplePost[0])
 
     return render_template( 'home.html', 
-    posts = examplePost, 
+    postOne = examplePost[0], postTwo = examplePost[1], postThree = examplePost[2],
     title="Dev-Thep Blogging Platform",
     quote = random_quote['quote'], 
     author = random_quote['author'], 
